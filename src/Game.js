6ff083ffa999d969c42b10c70a7542734a50b121
unknown <juliansotoponce@gmail.com>
2018-11-18 17:46:40 -0800

    Clear default CRA app

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <p>Hello!</p>
+    );
+  }
+}
+
+export default Game;
